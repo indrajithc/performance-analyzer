32848b215a67f83e76491fb0144c1243338a563a
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-11-24 16:30:22 +0530

    refactor: toast added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import ReportPage from "./pages/ReportPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/report" element={<ReportPage/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/report" element={<ReportPage />} />
         </Routes>
       </BrowserRouter>
+      <ToastContainer />
     </div>
   );
 }
