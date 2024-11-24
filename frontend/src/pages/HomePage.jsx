9a8df69da526ebd1728752f3b62d12eb192c7206
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-11-24 17:40:59 +0530

    refactor: LighthouseAnalyzer in use

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useMemo, useState } from "react";
 import { getCookie } from "../utils/CookieUtil";
 import { SERVER_URL } from "../utils/constants";
 import BackendUrlReader from "../components/BackendUrlReader";
+import LighthouseAnalyzer from "../components/LighthouseAnalyzer";
 
 const HomePage = () => {
   const defaultServerApi = useMemo(() => getCookie(SERVER_URL) || null, []);
@@ -11,7 +12,7 @@ const HomePage = () => {
   return (
     <div>
       {serverApi ? (
-        <h1>Server URL: {serverApi}</h1>
+        <LighthouseAnalyzer serverApi={serverApi} />
       ) : (
         <>
           <BackendUrlReader onSetServerApi={setServerApi} />
