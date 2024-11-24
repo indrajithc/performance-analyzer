2f509e1d7a5b1bf8ec75b1d3d7622b6580c58676
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-11-24 16:29:58 +0530

    refactor: switch logic added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,24 @@
-
+import { useMemo, useState } from "react";
+import { getCookie } from "../utils/CookieUtil";
+import { SERVER_URL } from "../utils/constants";
+import BackendUrlReader from "../components/BackendUrlReader";
 
 const HomePage = () => {
+  const defaultServerApi = useMemo(() => getCookie(SERVER_URL) || null, []);
 
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-}
+  const [serverApi, setServerApi] = useState(defaultServerApi);
+
+  return (
+    <div>
+      {serverApi ? (
+        <h1>Server URL: {serverApi}</h1>
+      ) : (
+        <>
+          <BackendUrlReader onSetServerApi={setServerApi} />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default HomePage;
